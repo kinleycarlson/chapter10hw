267e1f1be997a73fc051fe324b45af7129ae0e7e
Kinley <Kinley@test.com>
2022-09-22 21:54:46 -0500

    classes

---
+++ EmployeeClass.py
@@ -0,0 +1,11 @@
+class Employeee:
+
+    def __init__(self,name,ID,dep,title,salary):
+        self.__name = name
+        self.__ID = ID
+        self.__department = dep
+        self.__jobTitle = title
+        self.__salary = salary
+
+
+
---
+++ PayrollDeductionClass.py
@@ -0,0 +1,7 @@
+class PayrollDeduction:
+
+    def __init__(self,desc,date,amt,ID):
+        self.__desc = desc
+        self.__date = date
+        self.__charge = amt
+        self.__employeeID = ID
